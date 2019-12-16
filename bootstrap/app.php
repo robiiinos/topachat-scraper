d11c1f8ecd82026509529b1cecf57ed19332b4b9
Robiiinos <dyrda.robin@outlook.fr>
2019-12-16 11:07:50 +0100

    Clean unused middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ $app = new Laravel\Lumen\Application(
 
 // $app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -58,11 +58,7 @@ $app->singleton(
 */
 
 // $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
-
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
+//     App\Http\Middleware\Middleware::class
 // ]);
 
 /*
