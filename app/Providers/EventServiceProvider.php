0537f98db0f1010c5fb77d0c54237e616278cedc
Robiiinos <dyrda.robin@outlook.fr>
2019-12-16 12:24:34 +0100

    Clean unused Lumen scaffolders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -12,8 +12,6 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\ExampleEvent' => [
-            'App\Listeners\ExampleListener',
-        ],
+        //
     ];
 }
